c67bb2fccee32c5639be8e226b1abbe06a60c2af
NormPlum <162703454+NormPlum@users.noreply.github.com>
2026-02-16 14:00:01 +1100

    Create 051.py

---
+++ 051.py
@@ -0,0 +1,4 @@
+# https://www.freecodecamp.org/learn/daily-coding-challenge/2025-09-30
+
+def format_number(number):
+    return "+" + number[0] + " (" + number[1:4] + ") " + number[4:7] + "-" + number[7:]
