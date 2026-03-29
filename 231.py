de5aa38430ad80187d5d9c68a565862c4c34680a
NormPlum <162703454+NormPlum@users.noreply.github.com>
2026-03-30 08:32:17 +1100

    Create 231.py

---
+++ 231.py
@@ -0,0 +1,22 @@
+# https://www.freecodecamp.org/learn/daily-coding-challenge/2026-03-29
+
+import re
+
+def is_valid_isbn10(s):
+    match = re.search("^[0-9X-]{13}$", s)
+    if not match:
+        return False
+
+    characters = re.sub("-", "", s)
+    total = 0
+    for i, char in enumerate(characters):
+        pos = i + 1
+        if char == "X":
+            total += (10 * pos)
+        else:
+            total += (int(char) * pos)
+
+    if total % 11 == 0:
+        return True
+    else:
+        return False
