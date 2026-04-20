40512201e407ce8d9f8cb3db0a539febf549e610
NormPlum <162703454+NormPlum@users.noreply.github.com>
2026-04-21 09:21:47 +1000

    Create 253.py

---
+++ 253.py
@@ -0,0 +1,13 @@
+# https://www.freecodecamp.org/learn/daily-coding-challenge/2026-04-20
+
+def find_org(acronym):
+    acronyms = {
+        "NASA": "National Avocado Storage Authority",
+        "CIA": "Cats Infiltration Agency",
+        "FBI": "Fluffy Beanbag Inspectors",
+        "DOJ": "Department Of Jelly",
+        "WHO": "Wild Honey Organization",
+        "EPA": "Eating Pancakes Administration",
+    }
+
+    return acronyms[acronym]
